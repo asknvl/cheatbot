70a8451db0c1e45d76a4ed981dbc74cffb4e9b1d
Alexey Konovalov <asknvl@gmail.com>
2024-01-07 21:52:21 +0300

    .4:

--- cheatbot/Models/drop/Drop_v0.cs
+++ cheatbot/Models/drop/Drop_v0.cs
@@ -1,5 +1,6 @@
 ﻿using asknvl;
 using asknvl.logger;
+using cheatbot.Models.reactions;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using ReactiveUI;
@@ -20,23 +21,25 @@ namespace cheatbot.Models.drop
         #region vars
         System.Timers.Timer readHistoryTimer;
         List<messageInfo> newMessagesQueue = new();
+        ReactionsStateManager reactionsMansger = ReactionsStateManager.getInstance();
+        Random r = new Random();
         #endregion
 
         public Drop_v0(string api_id, string api_hash, string phone_number, string old_2fa_password, ILogger logger) : base(api_id, api_hash, phone_number, old_2fa_password, logger)
         {
         }
 
-        Random r = new Random();
-
         bool needFirstWatch = false;
         async void ReadHistoryTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
 
-            if (needFirstWatch) {
+            if (needFirstWatch)
+            {
 
                 needFirstWatch = false;
 
-                await Task.Run(async () => { 
+                await Task.Run(async () =>
+                {
 
                     foreach (var channel in chats.chats)
                     {
@@ -60,8 +63,6 @@ namespace cheatbot.Models.drop
 
                 });
 
-
-                
             }
 
 
@@ -85,18 +86,19 @@ namespace cheatbot.Models.drop
                     foreach (var item in tmpList)
                         newMessagesQueue.Remove(item);
 
-                    var messages = tmpList.Select(m => m.message_id).ToArray();
+                    var messagesIDs = tmpList.Select(m => m.message_id).ToArray();
 
-                    var v = await user.Messages_GetMessagesViews(channel, messages, true);
+                    var v = await user.Messages_GetMessagesViews(channel, messagesIDs, true);
 
                     //var v = await user.Messages_GetMessagesViews(channel, new int[] { m.message_id }, true);
                     //newMessagesQueue.RemoveAt(0);
-                    SendChannelMessageViewedEvent(m.peer_id, (uint)messages.Length);
+                    SendChannelMessageViewedEvent(m.peer_id, (uint)messagesIDs.Length);
                     //logger.err(phone_number, $"Viewed {m.message_id}");
 
                     int percentage = r.Next(1, 100);
 
-                    if (test_mode && percentage <= 20)                    {
+                    if (/*test_mode && percentage <= 20*/true)
+                    {
 
                         var reactions = fullChats.FirstOrDefault(c => c.chats.ContainsKey(channel.ID))?.full_chat.AvailableReactions;
 
@@ -108,32 +110,75 @@ namespace cheatbot.Models.drop
 
                         if (reactions is ChatReactionsSome)
                         {
+
                             var some = (ChatReactionsSome)reactions;
                             var available = some.reactions;
+                            
+                            //int selected = 0;
+                            //percentage = r.Next(1, 100);
 
-                            int selected = 0;
-                            percentage = r.Next(1, 100);
+                            //if (percentage <= 40)
+                            //    selected = 0;
+                            //else
+                            //    if (available.Length > 2 && percentage > 40 && percentage <= 70)
+                            //    selected = 1;
+                            //else
+                            //    if (available.Length > 3)
+                            //    selected = r.Next(2, available.Length - 1);
 
-                            if (percentage <= 40)
-                                selected = 0;
-                            else
-                                if (available.Length > 2 && percentage > 40 && percentage <= 70)
-                                selected = 1;
-                            else
-                                if (available.Length > 3)
-                                selected = r.Next(2, available.Length-1);
-
-                            foreach (var message in messages)
+                            foreach (var messageID in messagesIDs)
                             {
-                                await user.Messages_SendReaction(channel, message, reaction: new[] { available[selected] });
-                                await Task.Delay(5000);
+                                try
+                                {
+                                    reactionsMansger.UpdateMessageList(channel.ID, messageID, available);
+                                } catch (Exception ex)
+                                {
+                                    logger.err("ReadHistoryTimer View:", ex.Message);
+                                }
+
+                                var messageState = reactionsMansger.Get(channel.ID, messageID);
+
+                                if (messageState != null)
+                                {
+
+
+                                    Reaction[]? randomizedReactions = messageState.reactions;
+
+                                    string s = "";
+                                    foreach (var reaction in randomizedReactions)
+                                    {
+                                        s = s + $" {((ReactionEmoji)reaction)?.emoticon}";                                     
+                                    }
+                                    logger.inf("rndmized:", s);
+
+                                    if (randomizedReactions.Length > 0)
+                                    {
+
+                                        int selected = 0;
+                                        percentage = r.Next(1, 100);
+
+                                        if (percentage <= 40)
+                                            selected = 0;
+                                        else
+                                            if (available.Length > 2 && percentage > 40 && percentage <= 70)
+                                            selected = 1;
+                                        else
+                                            if (available.Length > 3)
+                                            selected = r.Next(2, available.Length - 1);
+
+                                        await user.Messages_SendReaction(channel, messageID, reaction: new[] { randomizedReactions[selected] });
+                                        await Task.Delay(5000);
+
+                                    }
+                                }
                             }
 
                         }
                     }
                 }
 
-            } catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 if (newMessagesQueue.Count > 0)
                     newMessagesQueue.RemoveAt(0);
@@ -159,6 +204,9 @@ namespace cheatbot.Models.drop
                     {
                         var msgInfo = new messageInfo(unm);
                         newMessagesQueue.Add(msgInfo);
+
+                        logger.inf(phone_number, $"ch:{msgInfo.peer_id} msg:{msgInfo.message_id}");
+
                     }
                     break;
 
@@ -166,7 +214,8 @@ namespace cheatbot.Models.drop
                     try
                     {
                         chats = await user.Messages_GetAllChats();
-                    } catch (Exception ex)
+                    }
+                    catch (Exception ex)
                     {
                         logger.err(phone_number, $"processUpdate: {ex.Message}");
                     }
@@ -176,19 +225,20 @@ namespace cheatbot.Models.drop
 
         public override Task Start()
         {
-            return base.Start().ContinueWith(t => {
+            return base.Start().ContinueWith(t =>
+            {
 
                 if (is_active)
                 {
 
-                    Random r = new Random();
+                    //Random r = new Random();
 
                     //int offset = (r.Next(1, 9) * 1000 + r.Next(1, 10) * 100) * 60;
                     //double minOffset = (double)offset / 1000 / 60;
 
 
                     double minOffset = r.Next(1, 10) + (1.0d * r.Next(1, 10) / 10);
-                    int offset = (int)(minOffset * 60 * 1000);
+                    int offset = (int)(minOffset * 1 * 1000);
 
                     logger.inf("", $"minoffset={minOffset} offset={offset}");
 
@@ -207,14 +257,15 @@ namespace cheatbot.Models.drop
 
         public override async Task Stop()
         {
-            await base.Stop().ContinueWith(t => {
+            await base.Stop().ContinueWith(t =>
+            {
                 if (readHistoryTimer != null)
                 {
                     readHistoryTimer?.Stop();
                     readHistoryTimer.Elapsed -= ReadHistoryTimer_Elapsed;
                 }
             });
-            
+
         }
     }
 
---
+++ cheatbot/Models/reactions/ReactionsStateManager.cs
@@ -0,0 +1,89 @@
+﻿using cheatbot.Models.drop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TL;
+
+namespace cheatbot.Models.reactions
+{
+    public class ReactionsStateManager
+    {
+        #region const
+        int max_messages_states = 2048;
+        #endregion
+
+        #region vars
+        List<messageState> messageStates = new(); 
+        #endregion
+
+        #region singletone
+        private static ReactionsStateManager instance;
+        private ReactionsStateManager() { 
+        }
+
+        public static ReactionsStateManager getInstance()
+        {
+            if (instance == null)
+                instance = new ReactionsStateManager();
+            return instance;
+        }
+        #endregion
+
+        #region public        
+        public void UpdateMessageList(long channelID, int messageID, Reaction[] available)
+        {
+            var found = messageStates.Any(ms => ms.channelID == channelID && ms.messageID == messageID);
+            if (!found)
+            {
+                try
+                {
+                    var messageState = new messageState(channelID, messageID, available);
+                    messageStates.Add(messageState);
+                    if (messageStates.Count > max_messages_states)
+                        messageStates.RemoveAt(0);
+                } catch (Exception ex)
+                {
+                    throw new Exception($"UpdateMessageList {ex.Message}");
+                }
+            }
+        }
+
+        public messageState? Get(long channelID, int messageID) {
+            var found = messageStates.FirstOrDefault(ms => ms.channelID == channelID && ms.messageID == messageID);
+            return found;
+        }
+        #endregion
+    }
+
+    public class messageState
+    {
+        public long channelID { get; set; }
+        public int messageID { get; set; }
+        public Reaction[] reactions { get; set; } = new Reaction[0];
+
+        public messageState(long channelID, int messageID, Reaction[] available_reactions)
+        {
+            this.channelID = channelID;
+            this.messageID = messageID;
+            reactions = getReactionsSeq(available_reactions);
+        }   
+
+        Reaction[] getReactionsSeq(Reaction[] available)
+        {
+            
+            Reaction[] res = new Reaction[available.Length];
+
+            Random r = new Random();
+            int[] randSeq = Enumerable.Range(0, available.Length).OrderBy(x => r.Next()).ToArray();
+
+            for (int index = 0; index < randSeq.Length; index++)
+            {
+                res[index] = available[randSeq[index]];
+            }
+            return res;
+        }
+    }
+         
+}
